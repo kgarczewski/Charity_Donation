76740af3a0e062747beef78a700f63b449d00e68
k.garczewski <garczewskikrzysztof@gmail.com>
2022-09-16 10:02:52 +0200

    Adding docstrings.

--- portfolio_lab/settings.py
+++ portfolio_lab/settings.py
@@ -144,7 +144,9 @@ TIME_INPUT_FORMATS = ('%H:%M',)
 # Emailing settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-
+EMAIL_FROM = ''
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
